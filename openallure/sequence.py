fc38bff1d478e2d8113296ae9f32b52c79833900
John Graves <John Graves@localhost>
2010-02-27 01:48:14 +1300

    Version 0.1d1 of Open Allure.

--- openallure/sequence.py
+++ openallure/sequence.py
@@ -6,26 +6,30 @@ Parses separate content file
 
 **Usage**
 
-sequence(filename) returns a sequence object
+Sequence( *filename* ) returns a sequence object
+
+*filename* can be either a local file or a URL
 
 An input file is plain text with the format::
 
    Question part1
-   [optional Question part2]
-   [optional blank line]
+   [ optional Question part2 ]
+   [ optional blank line ]
    Answer 1 <separator> Response 1
    Answer 2 <separator> Response 2
     etc ...
    up to 6 answers
-   [blank line]
+   [ blank line ]
    Next question ...
 
 where <separator> can be::
 
    ;                no action
    ;; or ;1 or ;+1  advance to next question
-   ;-1              return to prior question (in order exposed in sequence)
+   ;-1              return to prior question ( in order exposed in sequence )
    ;;; or ;2 or ;+2 advance two questions
+   ;[filename]      advance to first question found in filename
+   ;[url]           advance to first question found in text marked <pre> </pre> at URL (webpage)
 
 
 **Output**
@@ -33,105 +37,139 @@ where <separator> can be::
 List of lists::
 
    #   [   The whole sequence of questions is outermost list,
-   #                             so seq[0] is everything about the first question
-   #    [  The parts of the a question including the question set, answer set, response set and action set are next level list,
-   #                             so seq[ ][0] is the question set
-   #                                seq[ ][1] is the answer set
-   #                                seq[ ][2] is the response set
-   #                                seq[ ][3] is the action set
+   #                             so seq[ 0 ] is everything about the first question
+   #    [  The parts of the a question including the question set, answer set, response set and action/destination sets are the next level list,
+   #                             so seq[ ][ 0 ] is the question set
+   #                                seq[ ][ 1 ] is the answer set
+   #                                seq[ ][ 2 ] is the response set
+   #                                seq[ ][ 3 ] is the action set
+   #                                seq[ ][ 4 ] is the action set destinations (filenames or URLs)
    #     [ The parts of the question are the next level list,
-   #                             so seq[ ][0][0] is the first part of the question, for example "What color"
-   #                            and seq[ ][0][1] is the next  part of the question, for example "is the sky?" ],
+   #                             so seq[ ][ 0 ][ 0 ] is the first part of the question, for example "What color"
+   #                            and seq[ ][ 0 ][ 1 ] is the next  part of the question, for example "is the sky?" ],
    #     [ The answers are the next list,
-   #                             so seq[ ][1][0] is the first  answer, for example "Black"
-   #                            and seq[ ][1][1] is the second answer, for example "Blue" ],
+   #                             so seq[ ][ 1 ][ 0 ] is the first  answer, for example "Black"
+   #                            and seq[ ][ 1 ][ 1 ] is the second answer, for example "Blue" ],
    #     [ The response are the next list,
-   #                             so seq[ ][2][0] is the first  response, for example "Yes, at night."
-   #                            and seq[ ][2][1] is the second response, for example "Yes, during the day." ],
+   #                             so seq[ ][ 2 ][ 0 ] is the first  response, for example "Yes, at night."
+   #                            and seq[ ][ 2 ][ 1 ] is the second response, for example "Yes, during the day." ],
    #     [ The actions are the next list,
-   #                             so seq[ ][3][0] is the first  action, for example 0 (meaning take no action)
-   #                            and seq[ ][3][1] is the second action, for example 1 (meaning advance one question) ]]]
+   #                             so seq[ ][ 3 ][ 0 ] is the first  action, for example 0 ( meaning take no action )
+   #                            and seq[ ][ 3 ][ 1 ] is the second action, for example 1 ( meaning advance one question ) ],
+   #     [ The destinations are the next list,
+   #                             so seq[ ][ 4 ][ 0 ] is the first  destination, for example 'secondSetOfQuestions.txt'
+   #                            and seq[ ][ 4 ][ 1 ] is the second destination, for example 'http://bit.ly/openalluretest' ]]]
 
 See `Open Allure wiki`_ for details and examples.
 
 .. _Open Allure wiki: http://code.google.com/p/open-allure-ds/wiki/SeparateContentFileSyntax
 
 Copyright (c) 2010 John Graves
+
 MIT License: see LICENSE.txt
 """
 
-class Sequence(object):
+import urllib
+import ConfigParser
+from BeautifulSoup import BeautifulSoup          # For processing HTML
 
-    def classify(self,strings):
+class Sequence( object ):
+
+    def __init__( self, filename="openallure.txt" ):
+        """
+        Read either a local plain text file or text tagged <pre> </pre> from a webpage
+        """
+        if filename[ :4 ] == "http":
+           # read text tagged with <pre> </pre> from website
+           fromWeb = BeautifulSoup( ''.join( urllib.urlopen( filename ) ) ).pre.getText( separator=u"\n" )
+           inputs = fromWeb.splitlines()
+        else:
+           # read file
+           inputs = open( filename ).readlines()
+
+        # parse into sequence
+        self.sequence = self.regroup( inputs, self.classify( inputs ) )
+
+    def classify( self,strings ):
         """
 Create list of string types::
 
-   Identify strings which contain new line only   (type N)
-   #             or which contain ; or ;; markers (type indicated by offset of marker between Answer ; Response)
-   #             or else mark as question         (type Q)
+   Identify strings which contain new line only   ( type N )
+   #             or which contain ; or ;; markers ( type indicated by offset of marker between Answer ; Response )
+   #             or else mark as question         ( type Q )
 
         """
         string_types = []
         for i in strings:
-            if i.rstrip() == "": string_types.append("N")
+            if i.rstrip() == "": string_types.append( "N" )
             else:
-               slash_at = i.find(";")
-               if slash_at > 0:  string_types.append(str(slash_at))
-               else:             string_types.append("Q")
+               slash_at = i.find( ";" )
+               if slash_at > 0:  string_types.append( str( slash_at ) )
+               else:             string_types.append( "Q" )
 
         return string_types
 
-    def regroup(self,strings,string_types):
+    def regroup( self,strings,string_types ):
         """Use string_types to sort strings into Questions,
                                     Answers, Responses and Subsequent Actions"""
-        on_string = 0
-        sequence = []
-        question = []
-        answer   = []
-        response = []
-        action   = []
-        while on_string < len(strings):
-            if string_types[on_string] == "Q": question.append(strings[on_string].rstrip())
-            elif string_types[on_string] == "N":
+        onString    = 0
+        sequence    = []
+        question    = []
+        answer      = []
+        response    = []
+        action      = []
+        destination = []
+        while onString < len( strings ):
+            if string_types[ onString ] == "Q": question.append( strings[ onString ].rstrip() )
+            elif string_types[ onString ] == "N":
                 # signal for end of question IF there are responses
-                if len(response):
+                if len( response ):
                     # add to sequence and reset
-                    sequence.append([question, answer, response, action])
-                    question = []
-                    answer   = []
-                    response = []
-                    action   = []
+                    sequence.append( [question, answer, response, action, destination] )
+                    question    = []
+                    answer      = []
+                    response    = []
+                    action      = []
+                    destination = []
             else:
                 # use number to break string into answer and response
-                answer.append(strings[on_string][:int(string_types[on_string])].rstrip())
-                # NOTE: +1 means to leave off first ;
-                response_string = strings[on_string][int(string_types[on_string])+1:].strip()
+                answer.append( strings[ onString ][ :int( string_types[ onString ] ) ].rstrip() )
+                # NOTE: +1 means to leave off first semi-colon
+                responseString = strings[ onString ][ int( string_types[ onString ] ) + 1: ].strip()
 
-                # examine start of response_string to determine if it signals action
-                # with additional ;'s or digits (including + and -)
+                # examine start of responseString to determine if it signals action
+                # with additional ;'s or digits ( including + and - ) or brackets
                 # IF none found, leave action as 0
-                action_value = 0
-                while len(response_string) and response_string[0] == ';':
-                    action_value += 1
-                    response_string = response_string[1:].lstrip()
+                actionValue = 0
+                while len( responseString ) and responseString[ 0 ] == ';':
+                    actionValue += 1
+                    responseString = responseString[ 1: ].lstrip()
                 digits = ''
-                while len(response_string) and (response_string[0].isdigit() or response_string[0] in ['+','-']):
-                    digits += response_string[0]
-                    response_string = response_string[1:].lstrip()
-                if len(digits): action_value = int(digits)
+                while len( responseString ) and ( responseString[ 0 ].isdigit() or responseString[ 0 ] in ['+','-'] ):
+                    digits += responseString[ 0 ]
+                    responseString = responseString[ 1: ].lstrip()
+                if len( digits ): actionValue = int( digits )
+                if responseString[ 0 ] == '[':
+                    linkEnd = responseString.find(']')
+                    link    = responseString[1:linkEnd]
+                    responseString = responseString[ linkEnd + 1: ].lstrip()
+                    actionValue = 88
+                    # now look at link and decide whether it is a page name that needs help to become a URL
+                    if not ( link[:4] == "http" or link[ :-4 ] == '.txt' ):
+                        # get start of URL from .cfg file
+                        config = ConfigParser.RawConfigParser()
+                        config.read( 'openallure.cfg' )
+                        url = config.get( 'Source', 'url' )
+                        slashAt = url.rfind( '/' ) + 1
+                        link = url[0:slashAt] + link
+                    destination.append( link )
+                else:
+                    destination.append( "" )
 
-                response.append(response_string)
-                action.append(action_value)
+                response.append( responseString )
+                action.append( actionValue )
 
-            on_string += 1
+            onString += 1
         # append last question if not already signaled by N at end of inputs
-        if len(question): sequence.append([question, answer, response, action])
+        if len( question ): sequence.append( [question, answer, response, action, destination] )
         return sequence
-
-    def __init__(self, filename="openallure.txt"):
-        # read file
-        inputs = open(filename).readlines()
-
-        # parse into sequence
-        self.sequence = self.regroup(inputs, self.classify(inputs))
-
