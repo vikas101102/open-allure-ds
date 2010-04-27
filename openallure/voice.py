5ab8e9afe6886bafeaf50bb00220c669969f909d
John Graves <John Graves@localhost>
2010-04-27 16:39:59 +1200

    Open Allure 0.1d14

--- openallure/voice.py
+++ openallure/voice.py
@@ -14,10 +14,13 @@ TODO: Add standalone tests for text-to-speech modules.
 import os
 import ConfigParser
 import pygame
+
+# only allow useDragonfly if dragonfly module can be imported
+allowUseDragonfly = True
 try:
     import dragonfly
 except ImportError:
-    pass
+    allowUseDragonfly = False
 
 class Voice( object ):
     """Text-to-speech Functionality ( optional )"""
@@ -28,20 +31,30 @@ class Voice( object ):
         """
         config = ConfigParser.RawConfigParser()
         config.read( 'openallure.cfg' )
-        self.systemHasDragonfly = eval( config.get( 'Voice', 'systemHasDragonfly' ) )
-        self.systemHasEspeak    = eval( config.get( 'Voice', 'systemHasEspeak' ) )
-        self.systemHasSay       = eval( config.get( 'Voice', 'systemHasSay' ) )
+        if allowUseDragonfly:
+            self.useDragonfly = eval( config.get( 'Voice', 'useDragonfly' ) )
+        else:
+            self.useDragonfly = False
+            wantedToUseDragonfly = eval( config.get( 'Voice', 'useDragonfly' ) )
+            if wantedToUseDragonfly:
+                print "Dragonfly module not installed. Download from http://code.google.com/p/dragonfly/"
+
+        self.useEspeak = eval( config.get( 'Voice', 'useEspeak' ) )
+        self.useSay    = eval( config.get( 'Voice', 'useSay' ) )
+        self.language  = config.get( 'Voice', 'language' )
+        if self.language:
+            self.language = self.language + " "
 
     def speak( self, phrase ):
        """Say or print phrase using available text-to-speech engine or stdout"""
 
-       if self.systemHasDragonfly:
+       if self.useDragonfly:
            e = dragonfly.get_engine()
            e.speak( phrase.encode( 'utf-8' ) )
-       elif self.systemHasEspeak:
-           os.system( 'espeak -s150 "' + phrase.encode( 'utf-8' ) + '"' )
-       elif self.systemHasSay:
-           os.system( 'say "' + phrase.encode( 'utf-8' ) + '"' )
+       elif self.useEspeak:
+           os.system( 'espeak -s150 "' + language + phrase.encode( 'utf-8' ) + '"' )
+       elif self.useSay:
+           os.system( 'say "' + language + phrase.encode( 'utf-8' ) + '"' )
        else:
            print( phrase.encode( 'utf-8' ) )
            # Allow time for user to move hand down
